efe21e9070878228893f6015921639204c1e627c
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-09-04 17:50:02 +0100

    Implement NotFoundException

--- sergiosgc/rest/Exception.php
+++ sergiosgc/rest/Exception.php
@@ -2,3 +2,11 @@
 namespace sergiosgc\rest;
 
 class Exception extends \Exception { }
+
+class NotFoundException extends Exception {
+    public function __construct($message = "", $code = 0, $previous = NULL) {
+        if ($message == '') $message = 'Not Found';
+        header(sprintf('HTTP/1.0 404 %s', $message));
+        parent::__construct($message, $code, $previous);
+    }
+}
